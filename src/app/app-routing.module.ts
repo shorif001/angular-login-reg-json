5154a9fe24fe112fa861676607f4a4f60ee761c1
Shorif Ahmed <sashipon001@gmail.com>
2022-01-20 15:48:30 +0600

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { SignupComponent } from './components/signup/signup.component';
-import { AdminDashboardComponent } from './modules/admin/components/admin-dashboard/admin-dashboard.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  {path:'', redirectTo:'login', pathMatch:'full'},
-  {path:'login', component:LoginComponent},
-  {path:'signup', component:SignupComponent},
-  {path:'dashboard', component:AdminDashboardComponent},
+  {path: 'login', component:LoginComponent},
+  {path:'', redirectTo: 'login', pathMatch:'full'},
+  {path: 'forgot-password', component:ForgotPasswordComponent},
+  {path: 'signup', component:SignupComponent},
+  {path: 'admin', 
+  canActivate: [AuthGuard], 
+  loadChildren: ()=> import('./modules/admin/admin.module').then((m)=>m.AdminModule)},
+  {path: '**', component:NotFoundComponent},
 ];
 
 @NgModule({
