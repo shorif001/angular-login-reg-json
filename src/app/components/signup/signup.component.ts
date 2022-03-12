25a2dbcbc89d0aad37835ea079f655b52d5be103
Shorif Ahmed <sashipon001@gmail.com>
2022-03-12 10:13:09 +0600

    login registration field validation

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormBuilder} from '@angular/forms'
+import {FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms'
 import { Router } from '@angular/router';
 
 @Component({
@@ -16,12 +16,28 @@ export class SignupComponent implements OnInit {
 
   ngOnInit(): void {
     this.signupForm = this.formBuilder.group({
-      fullName:[''],
-      email:[''],
-      mobile:[''],
-      password:['']
+      fullName:new FormControl('', [Validators.required]),
+      email:new FormControl('', [Validators.required]),
+      mobile:new FormControl('', [Validators.required]),
+      password:new FormControl('', [Validators.required,Validators.minLength(5)])
     })
   }
+
+  //signup field validation start
+  get fullName(){
+    return this.signupForm.get('fullName');
+  }
+  get email(){
+    return this.signupForm.get('email')
+  }
+  get mobile(){
+    return this.signupForm.get('mobile')
+  }
+  get password(){
+    return this.signupForm.get('password')
+  }
+
+  //signup field validation end
 
   signUp(){
     this.http.post<any>('http://localhost:3000/signupUsers', this.signupForm.value)
